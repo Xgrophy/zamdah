a7a410cc3ed61b70f4fc9a192069e68de78446cf
Sᴋʏ Nᴏᴛ <92210483+Cangcimenn@users.noreply.github.com>
2022-03-21 19:15:35 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -60,7 +60,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Cangcimenn/musi",
+    "https://github.com/kenkansaja/Musikku",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Musikku")
 
